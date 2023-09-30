083f67f70472ef07f369b6dd39e73d280b5c65ca
BoljevicB <boljevic.1996@gmail.com>
2023-09-30 20:19:42 +0300

    Link to blog added on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,10 +7,10 @@ const Footer = () => {
 
     <nav className="flex justify-center flex-wrap gap-6 text-gray-500 font-medium">
     <Link to="/" className="hover:text-gray-900">Home</Link>
-    <Link to="/products" className="hover:text-[#1A5DAD]">Products</Link>
-    <Link to="/services" className="hover:text-[#1A5DAD]">Services</Link>
-    <Link to="/references" className="hover:text-[#1A5DAD]" >Reference</Link>
-        <a className="hover:text-gray-900" href="#">Blog</a>
+    <Link to="/products" className="hover:text-gray-900">Products</Link>
+    <Link to="/services" className="hover:text-gray-900">Services</Link>
+    <Link to="/references" className="hover:text-gray-900" >Reference</Link>
+        <Link to="/blog" className="hover:text-gray-900">Blog</Link>
         <a className="hover:text-gray-900" href="#">About</a>
         <a class="hover:text-gray-900" href="#">Contact</a>
     </nav>
