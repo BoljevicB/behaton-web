afdec783cbbd5d831003ba546e7292b079fba973
BoljevicB <boljevic.1996@gmail.com>
2023-10-03 21:49:58 +0300

    fixed hero margins

--- src/components/homepage/Hero.jsx
+++ src/components/homepage/Hero.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Hero = () => {
   return (
@@ -8,10 +9,10 @@ const Hero = () => {
     <div className="absolute inset-0 bg-black opacity-40"></div>
   </div>
   
-  <div className="relative z-10 flex flex-col justify-center items-center md:items-start h-full text-center p-4">
+  <div className="relative z-10 flex flex-col justify-center items-center md:items-start lg:items-start lg:ml-52 h-full text-center p-4">
     <h1 className="text-5xl font-bold leading-tight mb-4">Welcome to BEHAMONT</h1>
-    <p className="text-center lg:text-left text-lg text-gray-200 mb-8">Mi se bavimo proizvodnjom, <br/> projektovanjem i ugradnjom <br/> behaton ploča i ostalih betonskih elemenata.<br/> Našu gamu proizvoda čine: <br/> behaton ploče, ivičnjaci,rigole i betonske cevi. </p>
-    <a href="#" className="bg-white  text-gray-900 hover:bg-black hover:text-white py-2 px-6 rounded-lg border border-gray-400 text-lg font-semibold transition duration-300 transform hover:shadow-lg hover:border-gray-500">Contact Us</a>
+    <p className="text-center md:text-left lg:text-left text-lg text-gray-200 mb-8">Mi se bavimo proizvodnjom, <br/> projektovanjem i ugradnjom <br/> behaton ploča i ostalih betonskih elemenata.<br/> Našu gamu proizvoda čine: <br/> behaton ploče, ivičnjaci,rigole i betonske cevi. </p>
+    <Link to="/contact" className="bg-white  text-gray-900 hover:bg-black hover:text-white py-2 px-6 rounded-lg border border-gray-400 text-lg font-semibold transition duration-300 transform hover:shadow-lg hover:border-gray-500">Contact Us</Link>
   </div>
 </div>
 
